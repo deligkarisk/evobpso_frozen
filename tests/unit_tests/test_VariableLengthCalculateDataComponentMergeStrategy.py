fd80534bda302027e775224196e6e281ffac4b1e
deligkarisk <deligkarisk@gmail.com>
2023-04-25 13:11:31 +0900

    test for number of data calculator calls

---
+++ tests/unit_tests/test_VariableLengthCalculateDataComponentMergeStrategy.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from evobpso.velocity_factor.VelocityFactor import VelocityFactorEvolve, VelocityFactorRemove, VelocityFactorAdd
+from evobpso.velocity_update_strategy.component_merge_strategy.VariableLengthCalculateDataComponentMergeStrategy import \
+    VariableLengthCalculateDataComponentMergeStrategy
+
+
+class TestVariableLengthCalculateDataComponentMergeStrategy(TestCase):
+    def test_merge_personal_and_global_components_correct_number_of_calculator_calls(self):
+
+        component_a = VelocityFactorEvolve(data=0b100111)
+        component_b = VelocityFactorRemove()
+        component_d = VelocityFactorEvolve(data=0b100111)
+        component_c = VelocityFactorAdd(data=0b100111)
+
+        personal_component = [component_a, component_b, component_c, component_d]
+
+        component_a = VelocityFactorEvolve(data=0b111111)
+        component_b = VelocityFactorRemove()
+        component_d = VelocityFactorEvolve(data=0b000100)
+        global_component = [component_a, component_b, component_c, component_d]
+
+
+        mock_data_calculator = Mock()
+        pso_params = Mock()
+        pso_params.k = 1
+
+        component_merge_strategy = VariableLengthCalculateDataComponentMergeStrategy(component_merger_data_calculator=mock_data_calculator)
+        result = component_merge_strategy.merge_personal_and_global_components(personal_component=personal_component, global_component=global_component, pso_params=pso_params)
+
+        # There are two layers in the personal and global components that both are Evolve, so the calculator should have been called twice
+        assert mock_data_calculator.calculate.call_count == 2
